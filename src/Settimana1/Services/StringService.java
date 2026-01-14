8baf74060be80f6881a3734bf99e0f338a004898
academy0 <academy0@tutamail.com>
2026-01-14 17:52:58 +0100

    Quarto commit Niccolò
    Primi 7 metodi

--- src/Settimana1/Services/StringService.java
+++ src/Settimana1/Services/StringService.java
@@ -1,5 +1,8 @@
 package Settimana1.Services;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class StringService {
     /**
      * Questo metodo deve restituire la stringa passata in input con tutti i caratteri che la compongono in maiuscolo
@@ -16,7 +19,14 @@ public class StringService {
      * @param s
      * @return
      */
-    public String inverti(String s){return "prova";}
+    public String inverti(String s){
+        String inverted = "";
+
+        // ciclo i singoli caratteri da destra a sinistra
+        for(int i = s.length() -1; i>=0; i--){
+            inverted += s.charAt(i);
+        }
+        return inverted;}
 
 
     /**
@@ -25,7 +35,17 @@ public class StringService {
      * @return
      */
     public Integer contaVocali(String s){
-        return null;
+        int count = 0;
+
+        // Uso toCharArray per ottenere un'array di caratteri
+        // Uso una regex per verificare l'uguaglianza ma devo castare il carattere di nuovo a stringa
+        // Nota: probabilmente esiste un modo più efficiente
+        for(Character c: s.toCharArray()){
+            if(c.toString().matches("[AEIOUYaeiouy]")){
+                count++;
+            }
+        }
+        return count;
     }
 
     /**
@@ -35,7 +55,28 @@ public class StringService {
      * @return
      */
     public Boolean isPalindromo(String s){
-        return null;
+
+        // rimuovo gli spazi dalla stringa sostituendoli con una "stringa vuota"
+        // inoltre metto tutto in minuscolo per fare in modo che la stessa lettera maiuscola e minuscola non risulti diversa
+        String t = s.replace(" ","").toLowerCase();
+
+        // creo due contatori, uno che parte da sinistra e uno da destra
+        int i = 0;
+        int j = t.length()-1;
+
+        // confronto le lettere nella posizione dei contatori, se sono diverse la stringa non è palindroma
+        // se sono uguali sposto entrambi i contatori verso il centro e proseguo finché i contatori non sono nella stessa posizione
+        while(i<j){
+            if(t.charAt(i)!=t.charAt(j)){
+                return false;
+            }
+            else{
+                i++;
+                j--;
+            }
+        }
+        return true;
+
     }
 
     /**
@@ -45,7 +86,20 @@ public class StringService {
      * @return
      */
     public String capitalize(String s){
-        return null;
+
+        // splitto la stringa in un array con le singole parole (uso lo spazio come divisore)
+        String[] words = s.split(" ");
+        // creo una nuova stringa che avrà le iniziali maiuscole e le altre lettere minuscole
+        String capitalized = "";
+
+        // filtro ogni parola creandone una nuova con l'iniziale maiuscola e le altre lettere minuscole e riaggiungo lo spazio
+        for(String w: words){
+            String newWord = w.substring(0,1).toUpperCase().concat(w.substring(1).toLowerCase());
+            capitalized += newWord + " ";
+        }
+
+        // tronco l'ulitmo spazio
+        return capitalized.substring(0, capitalized.length()-1);
     }
 
     /**
@@ -55,7 +109,23 @@ public class StringService {
      * @return
      */
     public String rimuoviDuplicati(String s){
-        return null;
+
+        char[] arr = s.toCharArray();
+
+        String result = "";
+
+        // creo una stringa vuota
+        // filtro i caratteri della stringa originaria
+        // se non sono ancora contenuti nella stringa nuova li aggiungo altrimenti non succede nulla
+        // (non ci saranno così duplicati)
+        for (char c: arr){
+
+            if (!result.contains(Character.toString(c))){
+                result += c;
+            }
+        }
+
+        return result;
     }
 
     /**
@@ -65,7 +135,30 @@ public class StringService {
      * @return
      */
     public String comprimi(String s){
-        return null;
+        // *Minuscolizzo* la stringa
+        String min = s.toLowerCase();
+
+        // Creo una mappa dove i caratteri sono le chiavi e il conteggio di quante volte si ripetono il valore
+        Map<Character, Integer> characters = new HashMap<>();
+
+        // ciclo i caratteri della stringa, se è la prima volta che appaiono inserisco una coppia (nuovo carattere / 1)
+        // se esiste già sovrascrivo il valore di 1 (uso sempre put)
+        for(char c: min.toCharArray()){
+            if (!characters.containsKey(c)){
+                characters.put(c,1);
+            }
+            else{
+                characters.put(c,(characters.get(c))+1);
+            }
+        }
+
+        String message = "";
+
+        // popolo la stringa scremando la mappa aggiungendo prima la chiave e poi il valore per ogni coppia
+        for(char c: characters.keySet()){
+            message = message.concat(Character.toString(c).concat(Integer.toString(characters.get(c))));
+        }
+        return message;
     }
 
     /**
